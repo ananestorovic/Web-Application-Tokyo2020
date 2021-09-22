03a69083e6bc77cdb225ebeba2db4f86bf770131
ananestorovic <ananestorovic28@gmail.com>
2021-09-22 04:54:06 +0200

    logout done

--- frontend/app/src/app/delegate/delegate.component.ts
+++ frontend/app/src/app/delegate/delegate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delegate',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DelegateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logOut(){
+    localStorage.clear();
+    this.router.navigate(['']);
   }
 
 }
